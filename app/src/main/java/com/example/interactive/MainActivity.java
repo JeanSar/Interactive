a6f68452ff6b3c849eaeb94a90696b90c70e1aef
JeanSar <jeansar504@gmail.com>
2021-06-09 15:36:32 +0200

    Test pour git

--- app/src/main/java/com/example/interactive/MainActivity.java
+++ app/src/main/java/com/example/interactive/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Test pour git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
